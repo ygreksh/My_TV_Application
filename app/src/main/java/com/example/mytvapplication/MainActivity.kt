5bfa78bc178d1ab0b3a96fc4e8ca74c9d82862d9
ygrek <ygreksh@gmail.com>
2025-05-05 14:10:17 +0300

    1

--- app/src/main/java/com/example/mytvapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytvapplication/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.mytvapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.tv.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Button
+import androidx.tv.material3.ButtonBorder
+import androidx.tv.material3.ButtonColors
+import androidx.tv.material3.ButtonDefaults
+import androidx.tv.material3.ButtonGlow
+import androidx.tv.material3.ButtonScale
+import androidx.tv.material3.ButtonShape
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
 import com.example.mytvapplication.ui.theme.MyBlue
@@ -45,15 +66,53 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .fillMaxSize()
+//            .fillMaxSize()
             .background(MyTVBlack)
     ) {
-        Text(
-            text = "Hello $name!",
-            color = MyTVWhite,
-//            color = MyGreen,
-            modifier = modifier
-        )
+        Column {
+            Text(
+                text = "Hello $name!",
+                color = MyTVWhite,
+                modifier = modifier
+            )
+            Button(
+                onClick = { Log.d("test", "Button 1 onClick") }
+            ) {
+                Text(
+                    text = "Button 1",
+//                    color = MyTVWhite,
+//                    modifier = modifier
+                )
+            }
+            Button(
+                onClick = { Log.d("test", "Button 2 onClick") }
+            ) {
+                Text(
+                    text = "Button 2",
+                )
+            }
+            Button(
+                onClick = { Log.d("test", "Button 3 onClick") }
+            ) {
+                Text(
+                    text = "Button 3",
+                )
+            }
+            TvButton(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+                onClick = { Log.d("test", "Button 3 onClick") },
+            ) {
+                Text(
+//                    modifier = Modifier.fillMaxWidth(),
+                    text = "LOGIN",
+                    style = TextStyle(
+                        fontFamily = FontFamily.SansSerif,
+                        fontWeight = FontWeight.Light,
+                        textAlign = TextAlign.Center,
+                    ),
+                )
+            }
+        }
     }
 }
 
@@ -63,4 +122,35 @@ fun GreetingPreview() {
     MyTVApplicationTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun TvButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    scale: ButtonScale = ButtonDefaults.scale(),
+    glow: ButtonGlow = ButtonDefaults.glow(),
+    shape: ButtonShape = ButtonDefaults.shape(),
+    colors: ButtonColors = ButtonDefaults.colors(),
+    tonalElevation: Dp = 0.dp,
+    border: ButtonBorder = ButtonDefaults.border(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        scale = scale,
+        glow = glow,
+        shape = shape,
+        colors = colors,
+        tonalElevation = tonalElevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+        content = content,
+    )
 }
