c6de934d106b606ab434aa87f15a80a9a74b991c
ygrek <ygreksh@gmail.com>
2025-05-14 15:18:33 +0300

    + some screens and buttons

--- app/src/main/java/com/example/mytvapplication/Home2Screen.kt
+++ app/src/main/java/com/example/mytvapplication/Home2Screen.kt
@@ -1,7 +1,9 @@
 package com.example.mytvapplication
 
 import android.util.Log
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.tv.material3.Button
 import androidx.tv.material3.Text
 
 @Composable
@@ -22,9 +25,42 @@ fun Home2Screen(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Home2Screen",
-            color = Color.White
-        )
+//        Text(
+//            text = "Home2Screen",
+//            color = Color.White,
+//            modifier = Modifier.focusable()
+//        )
+        Column(
+        ) {
+            Text(
+                text = "Home2Screen",
+                color = Color.White
+            )
+
+            Button(
+                onClick = { Log.wtf("test", "HomeScreen Button1 onClick()")}
+            ) {
+                Text(
+                    text = "Button 1",
+                    color = Color.White
+                )
+            }
+            Button(
+                onClick = { Log.wtf("test", "HomeScreen Button2 onClick()")}
+            ) {
+                Text(
+                    text = "Button 2",
+                    color = Color.White
+                )
+            }
+            Button(
+                onClick = { Log.wtf("test", "HomeScreen Button3 onClick()")}
+            ) {
+                Text(
+                    text = "Button 3",
+                    color = Color.White
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mytvapplication/Movies2Screen.kt
+++ app/src/main/java/com/example/mytvapplication/Movies2Screen.kt
@@ -1,14 +1,19 @@
 package com.example.mytvapplication
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.tv.material3.Button
 import androidx.tv.material3.Text
 
 @Composable
@@ -22,9 +27,26 @@ fun Movies2Screen(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Movies2Screen",
-            color = Color.White
-        )
+//        Text(
+//            text = "Movies2Screen",
+//            color = Color.White
+//        )
+
+        Column(
+        ) {
+            Text(
+                text = "Movies2Screen",
+                color = Color.White
+            )
+
+            Button(
+                onClick = { Log.wtf("test", "Button1 onClick()")}
+            ) {
+                Text(
+                    text = "Button 1",
+                    color = Color.White
+                )
+            }
+        }
     }
 }
